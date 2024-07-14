918ee68a053a9cc887dae371d4d05e93d66d2b0c
Izazlsybna <izaz.alisyahbana@gmail.com>
2024-07-14 13:33:45 +0700

    Menghapus bacaan stok bycoding.com di tampilan karyawan

--- karyawan/comp/footer.php
+++ karyawan/comp/footer.php
@@ -4,7 +4,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="copyright">
-                                <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>. | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+                                <p>Copyright © 2018 Colorlib. All rights reserved.
                                 </p>
                             </div>
                         </div>
